2f69ed3fa5f04d0867a83ce516464c0252f03edb
Pushpendre Rastogi <pushpendre@gmail.com>
2017-01-16 03:20:36 -0500

    implement the new method for ranking points.

--- expand_contract/main.py
+++ expand_contract/main.py
@@ -4,20 +4,17 @@
 | Description :
 | Author      : Pushpendre Rastogi
 | Created     : Wed Jan 11 19:08:18 2017 (-0500)
-| Last-Updated: Thu Jan 12 00:56:52 2017 (-0500)
+| Last-Updated: Mon Jan 16 03:18:42 2017 (-0500)
 |           By: Pushpendre Rastogi
-|     Update #: 35
+|     Update #: 50
 '''
 from distance_computer import l2distance
 from schedule import Schedule
 import numpy as np
+import itertools
+from rasengan import tictoc
 
-
-def expand_contract_and_count(seed, point, D=None, sched=None):
-    if D is None:
-        D = l2distance(seed, point)
-    if sched is None:
-        sched = Schedule(auto_config=D)
+def expand_contract_and_count(seed, point, sched, D):
     # APPROACH 1
     # ret = np.zeros((point.shape[0], len(sched)), dtype='uint16')
     # from fast_count import count_mat_gt_radii
@@ -29,15 +26,32 @@ def expand_contract_and_count(seed, point, D=None, sched=None):
     #     ret[:, idx] = (D > radius).sum(axis=0)
 
     # APPROACH 3
-    from fast_count import layercake
-    ret = layercake(D, sched)
-    return ret
+    # from fast_count import layercake
+    # ret = layercake(D, sched)
+
+    # Approach 4
+    with tictoc('Step 1'):
+        M_tilde = np.searchsorted(sched, D)
+    with tictoc('Step 2'):
+        M_tilde.sort(axis=1)
+    assert M_tilde[0, 0] < M_tilde[0, 1]
+    def cmp_fnc(i, ip):
+        for j in xrange(M_tilde.shape[1]):
+            mij = M_tilde[i,j]
+            mipj = M_tilde[ip, j]
+            if mij > mipj:
+                return -1
+            elif mij < mipj:
+                return 1
+        return 0
+    with tictoc('Step 3'):
+        ranking = sorted(xrange(D.shape[0]), cmp=cmp_fnc)
+    return ranking
 
 
 if __name__ == '__main__':
     seed = np.random.rand(1000, 100)
-    point = np.random.rand(300000, 100)
-    D = l2distance(seed, point)
-    sched = Schedule(length=100, auto_config=D)
-    ret = expand_contract_and_count(seed, point, sched=sched, D=D)
-    print ret.shape, ret
+    point = np.random.rand(30000, 100)
+    D = l2distance(point, seed)
+    sched = Schedule(auto_config=D, keep_all=True)
+    ret = expand_contract_and_count(seed, point, sched, D)
--- expand_contract/schedule.py
+++ expand_contract/schedule.py
@@ -4,42 +4,34 @@
 | Description : A schedule of radius values
 | Author      : Pushpendre Rastogi
 | Created     : Wed Jan 11 18:33:46 2017 (-0500)
-| Last-Updated: Wed Jan 11 19:07:28 2017 (-0500)
+| Last-Updated: Mon Jan 16 02:46:45 2017 (-0500)
 |           By: Pushpendre Rastogi
-|     Update #: 5
+|     Update #: 10
 '''
 import numpy as np
 
 
-class Schedule(object):
+def create_linear_list(distmat, size):
+    mean = distmat[:].mean()
+    stdev = np.sqrt(distmat[:].var())
+    return np.linspace(mean - 2 * stdev, mean + 2 * stdev, num=size)
+
+
+def Schedule(stype='linear', length=10, auto_config=None, keep_all=True, lst=None):
     '''
     --- Example ---
     import numpy as np
     print list(Schedule(auto_config=np.array([[.1, .2], [.3, .4]])))
     '''
-    @staticmethod
-    def create_linear_list(distmat, size):
-        mean = distmat[:].mean()
-        stdev = np.sqrt(distmat[:].var())
-        return np.linspace(mean - 2 * stdev, mean + 2 * stdev, num=size)
-
-    def __init__(self, stype='linear', length=10, auto_config=None, **kwargs):
-        self.length = length
-        self.stype = stype
-        if auto_config is None:
-            self.lst = kwargs['lst']
+    if auto_config is None:
+        lst = lst
+    else:
+        assert isinstance(auto_config, np.ndarray)
+        if keep_all:
+            lst = np.unique(auto_config)
+            assert (len(lst) == 1) or (lst[0] <= lst[1])
+        elif stype == 'linear':
+            lst = create_linear_list(auto_config, length)
         else:
-            assert isinstance(auto_config, np.ndarray)
-            if stype != 'linear':
-                raise NotImplementedError(stype)
-            self.lst = self.create_linear_list(auto_config, length)
-        return
-
-    def __len__(self):
-        return len(self.lst)
-
-    def __iter__(self):
-        return iter(self.lst)
-
-    def __getitem__(self, idx):
-        return self.lst[idx]
+            raise NotImplementedError(stype)
+    return lst
