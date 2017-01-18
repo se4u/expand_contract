cbd931b626c779328d4ee77c8d0838a54d38641d
Pushpendre Rastogi <pushpendre@gmail.com>
2017-01-18 15:37:34 -0500

    finish handling ties.

--- expand_contract/main.py
+++ expand_contract/main.py
@@ -4,9 +4,9 @@
 | Description :
 | Author      : Pushpendre Rastogi
 | Created     : Wed Jan 11 19:08:18 2017 (-0500)
-| Last-Updated: Tue Jan 17 16:51:05 2017 (-0500)
+| Last-Updated: Wed Jan 18 15:33:13 2017 (-0500)
 |           By: Pushpendre Rastogi
-|     Update #: 70
+|     Update #: 102
 '''
 from distance_computer import l2distance
 from schedule import Schedule
@@ -32,6 +32,7 @@ def arrange_by_index(M, I):
 def inflation_ranking(sched, D, seed_labels):
     assert all(e in [-1, 1] for e in seed_labels)
     assert sched[0] < 0
+    assert np.all(D >= 0)
     with tictoc('Step 1'):
         M = np.searchsorted(sched, D)
     with tictoc('Step 2'):
@@ -42,20 +43,35 @@ def inflation_ranking(sched, D, seed_labels):
         G_tilde = (np.diff(M_tilde, axis=1)==0)
     assert M_tilde[0, 0] <= M_tilde[0, 1]
     def cmp_fnc(i, ip):
+        def get_nbr_influence(row_idx, j):
+            nbr_influence = seed_labels[I_tilde[row_idx, j]]
+            if j == G_tilde.shape[1]:
+                return nbr_influence
+            while G_tilde[row_idx, j]:
+                nbr_influence +=  seed_labels[I_tilde[row_idx, j+1]]
+                if j == G_tilde.shape[1] - 1:
+                    break
+                else:
+                    j += 1
+            return nbr_influence
+
         for j in xrange(M_tilde.shape[1]):
             mij, mipj = M_tilde[i,j], M_tilde[ip, j]
             if mipj < mij:
-                nbr_influence = seed_labels[I_tilde[ip, j]]
-                return (1 if nbr_influence < 0 else -1)
+                nbr_influence = get_nbr_influence(ip, j)
+                if nbr_influence != 0:
+                    return (1 if nbr_influence < 0 else -1)
             elif mipj > mij:
-                nbr_influence = seed_labels[I_tilde[i, j]]
-                return (1 if nbr_influence > 0 else -1)
+                nbr_influence = get_nbr_influence(i, j)
+                if nbr_influence != 0:
+                    return (1 if nbr_influence > 0 else -1)
             elif mij == mipj:
-                lij = seed_labels[I_tilde[i, j]]
-                lipj = seed_labels[I_tilde[ip, j]]
-                if lij != lipj:
-                    return (1 if lij > lipj else -1)
+                i_nbr_influence = get_nbr_influence(i, j)
+                ip_nbr_influence = get_nbr_influence(ip, j)
+                if i_nbr_influence != ip_nbr_influence:
+                    return (1 if i_nbr_influence > ip_nbr_influence else -1)
         return 0
+    assert cmp_fnc(0, 2) == -1
     with tictoc('Step 3'):
         ranking = sorted(xrange(D.shape[0]), cmp=cmp_fnc)
     return ranking
@@ -69,7 +85,7 @@ def test1(args):
     sched = Schedule(auto_config=D, keep_all=True)
     return sched, D, seed_labels
 
-def test2(_):
+def test2(*args):
     sched = np.array([-1, 1, 5, 9])
     D = np.array([[1, 1, 1, 1],
                   [5, 5, 1, 9],
